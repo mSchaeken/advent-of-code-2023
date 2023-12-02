c616e6302cd02924e941c7c44748db48cba1a502
mSchaeken <mathijs.schaeken@outlook.com>
2023-12-02 18:14:02 +0100

    Cleanly solve part two
    Definitely didn't brute force this :\

--- day01/trebuchet.py
+++ day01/trebuchet.py
@@ -5,17 +5,30 @@ from lib import lineParser
 #create new list with each line in file as separate item
 inputAsList = lineParser('./trebuchet.txt')
 
+#Don't judge me
+def wordsToDigit(string):
+    return string.lower()\
+    .replace('one', 'o1e')\
+    .replace('two', 't2o')\
+    .replace('three', 't3e')\
+    .replace('four', 'f4r')\
+    .replace('five', 'f5e')\
+    .replace('six', 's6x')\
+    .replace('seven', 's7n')\
+    .replace('eight', 'e8t')\
+    .replace('nine', 'n9e')\
+
 totalSum = 0
 #loop through each line in list
 for line in inputAsList:
+    formattedLine = wordsToDigit(line)
     digitsPerLine = []
     #append each digit in line to array
-    for character in line:
+    for character in formattedLine:
         if character.isdigit() == True:
             digitsPerLine.append(character)
     #cast first and last index of array joined together as int (also accounts for lines with just a single digit)
     digitsAsInt = int(digitsPerLine[0] + digitsPerLine[-1])
-    print(digitsAsInt)
     totalSum += digitsAsInt
 
 print('Total sum is ' + str(totalSum))
