9ba470c02e4ed1dbedd426a9d5d238e6515a176f
mSchaeken <mathijs.schaeken@outlook.com>
2023-12-02 17:30:04 +0100

    Remove digitsAsInt print statement

--- day01/trebuchet.py
+++ day01/trebuchet.py
@@ -3,7 +3,7 @@ sys.path.append('../lib')
 from lib import lineParser
 
 #create new list with each line in file as separate item
-inputAsList = lineParser('./trebuchetThier.txt')
+inputAsList = lineParser('./trebuchet.txt')
 
 totalSum = 0
 #loop through each line in list
@@ -15,9 +15,10 @@ for line in inputAsList:
             digitsPerLine.append(character)
     #cast first and last index of array joined together as int (also accounts for lines with just a single digit)
     digitsAsInt = int(digitsPerLine[0] + digitsPerLine[-1])
+    print(digitsAsInt)
     totalSum += digitsAsInt
 
-print(totalSum)
+print('Total sum is ' + str(totalSum))
 
 
 
