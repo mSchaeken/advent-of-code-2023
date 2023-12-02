398559e59fdfb76e5f1bd21d6b3bbf915c2a2237
mSchaeken <mathijs.schaeken@outlook.com>
2023-12-02 17:21:28 +0100

    Solve part 1

--- day01/trebuchet.py
+++ day01/trebuchet.py
@@ -3,7 +3,24 @@ sys.path.append('../lib')
 from lib import lineParser
 
 #create new list with each line in file as separate item
-inputAsList = lineParser('./trebuchet.txt')
+inputAsList = lineParser('./trebuchetThier.txt')
+
+totalSum = 0
+#loop through each line in list
+for line in inputAsList:
+    digitsPerLine = []
+    #append each digit in line to array
+    for character in line:
+        if character.isdigit() == True:
+            digitsPerLine.append(character)
+    #cast first and last index of array joined together as int (also accounts for lines with just a single digit)
+    digitsAsInt = int(digitsPerLine[0] + digitsPerLine[-1])
+    totalSum += digitsAsInt
+
+print(totalSum)
+
+
+
 
 
 
