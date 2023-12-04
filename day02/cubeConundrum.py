ef484c349f8aa3efa8f0447e3d834d1960881852
mSchaeken <mathijs.schaeken@outlook.com>
2023-12-04 16:57:28 +0100

    Go with different approach

--- day02/cubeConundrum.py
+++ day02/cubeConundrum.py
@@ -2,17 +2,51 @@ import sys
 sys.path.append('../lib')
 from lib import lineParser
 
-allGames = lineParser('./cubeConundrum.txt')
-#create empty list b
-grabsPerGame = []
-#for character in item from list a:
-for grab in allGames:
-    #split on semicoloms in each item and add to list b
-    grabsPerGame.append(grab.split(';'))
+maxCountPerGame = {
+    'red': 12,
+    'green': 13,
+    'blue': 14
+}
 
-print(grabsPerGame)
-#   append new separated items to empty list b
-#   for character in item from list b:
-#       create empty list c
-#       split on commas
-#       append to list c
+allGames = lineParser('./cubeConundrum.txt')
+grabsPerGame = []
+#format each line into comma separated items
+for grab in allGames:
+    grabsPerGame.append(grab.replace(';', ',')\
+                        .replace('\n', '')\
+                        .replace(':', ','))                  
+#print(grabsPerGame)
+                    
+#split on comma
+gamesSplitOnComma = []
+for grabs in grabsPerGame:
+        gamesSplitOnComma.append(grabs.split(','))
+print(gamesSplitOnComma)
+
+sumOfIds = 0
+id = 0
+for game in gamesSplitOnComma:   
+    gamePossible = True
+    for item in game:
+        if 'Game' in item:
+            id += 1        
+            continue
+        #if number with red higher than maxCount -> game impossible so break
+        if 'red' in item and int(item.strip('red ')) > maxCountPerGame['red']:            
+            #print('break on red')
+            gamePossible = False
+            break
+        #if number with green higher than maxCount -> game impossible so break
+        if 'green' in item and int(item.strip('green ')) > maxCountPerGame['green']:           
+            #print('break on green')
+            gamePossible = False
+            break
+        #if number with blue higher than maxCount -> game impossible so break
+        if 'blue' in item and int(item.strip('blue ')) > maxCountPerGame['blue']:            
+            #print('break on blue')
+            gamePossible = False
+            break
+    if gamePossible == True:
+         sumOfIds += id
+print(sumOfIds)
+    
