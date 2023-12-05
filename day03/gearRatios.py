6fc88cad312343b95a5841fe1f03a804d70576d0
mSchaeken <mathijs.schaeken@outlook.com>
2023-12-05 22:54:04 +0100

    Throw everything out the window and take different approach
    Because of my very limited knowledge of python syntax, since python is
    completely new to me, I went for an overly complicated initial idea.
    
    After some research I'll be going for a different approach and threw away
    the old code and pseudo code.

--- day03/gearRatios.py
+++ day03/gearRatios.py
@@ -1,34 +1,28 @@
 import sys
 sys.path.append('../lib')
 from lib import lineParser
-
-def numberJoiner(list):
-    numbersJoined = []
-    for item in list:
-        #set empty string to concatenate digits and counter (index)
-        numberString = ''
-        i = 0
-        #loop through all characters in item and check for digits -> add all digits to list
-        for character in item:
-            #if character is a digit add it to the number string
-            if character.isdigit():
-                numberString += str(character)
-                #if next character is not a number reset numberString to empty and add current string to list
-                if i + 1 < len(item) and item[i + 1].isdigit() == False:
-                    numbersJoined.append(int(numberString))
-                    numberString = ''
-            i += 1
-        numbersJoined.append(int(numberString))
-    return numbersJoined
-
-#testList = ['100.....15......21.....145....2']
-
-print(numberJoiner(testList))
-#parse every line
+import re
+                                         
 allLinesParsed = lineParser('./gearRatios.txt', True)
-#parse every character in line and save list with index of character and whether they are a number, special symbol or dot
-#loop through list, ignoring any dots, until special symbol is reached
-#then see if indexes to left and right of special symbol are a number
-#if true -> save that number
-#then loop through item containing entire line beneath the special symbol and see if any adjacent indexes (so same index, index-1 and index+1) are a number
-#if true -> save that number
+
+notSpecial = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+twoDimensionalLines = []
+for item in allLinesParsed:
+    oneDimensionalArray = []
+    oneDimensionalArray.append(item)
+    twoDimensionalLines.append(oneDimensionalArray)
+
+
+
+for count, line in enumerate(twoDimensionalLines):
+    #set current line in list
+    currentLine = twoDimensionalLines[count]
+    #set next line in list accounting for index range
+    if count + 1 < len(twoDimensionalLines):
+        nextLine = twoDimensionalLines[count + 1]
+    
+    for count, character in enumerate(currentLine[0]):
+        if character not in notSpecial:
+            print(character)
+         
