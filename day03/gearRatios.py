d8e1a945f0de30edf2ec90d5ccae2bae18fadb6a
mSchaeken <mathijs.schaeken@outlook.com>
2023-12-06 17:34:05 +0100

    Create algorithm to check for part numbers in same line

--- day03/gearRatios.py
+++ day03/gearRatios.py
@@ -6,6 +6,7 @@ import re
 allLinesParsed = lineParser('./gearRatios.txt', True)
 
 notSpecial = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+partNumberSum = 0
 
 twoDimensionalLines = []
 for item in allLinesParsed:
@@ -17,12 +18,33 @@ for item in allLinesParsed:
 
 for count, line in enumerate(twoDimensionalLines):
     #set current line in list
-    currentLine = twoDimensionalLines[count]
+    currentLine = twoDimensionalLines[count][0]
     #set next line in list accounting for index range
     if count + 1 < len(twoDimensionalLines):
         nextLine = twoDimensionalLines[count + 1]
     
-    for count, character in enumerate(currentLine[0]):
+    #loop through the item (string) in current line and look for special characters
+    for count, character in enumerate(currentLine):
         if character not in notSpecial:
-            print(character)
-         
+            #Initialise empty string and loop through characters
+            i = 1
+            numString = ''
+            while currentLine[count + i].isdigit() and (count + 1) + i < len(currentLine):
+                numString += currentLine[count + i]
+                i += 1
+                if currentLine[count + i].isdigit() == False:
+                    partNumberSum += int(numString)
+                    break
+
+            j = 1
+            numString = ''
+            while currentLine[count - j].isdigit() and (count - 1) - j < len(currentLine):
+                numString += currentLine[count - j]
+                j += 1
+                if currentLine[count - j].isdigit() == False:
+                    partNumberSum += int(numString)
+                    break
+
+print(partNumberSum)
+            
+            
