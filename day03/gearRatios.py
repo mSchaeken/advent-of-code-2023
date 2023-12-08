52dec0ac57c53c6f38c420b680fbcc334753e797
mSchaeken <mathijs.schaeken@outlook.com>
2023-12-08 17:02:22 +0100

    Continue later
    Python ranges make no sense to me. Will continue this challenge later.

--- day03/gearRatios.py
+++ day03/gearRatios.py
@@ -4,9 +4,13 @@ from lib import lineParser
 import re
                                          
 allLinesParsed = lineParser('./gearRatios.txt', True)
-
 notSpecial = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 partNumberSum = 0
+
+#function to look through an item in a list and split the item on digits
+def digitFinder(list):
+    digits = re.compile(r'(\d+)')
+    return digits.split(list[0])
 
 twoDimensionalLines = []
 for item in allLinesParsed:
@@ -14,37 +18,28 @@ for item in allLinesParsed:
     oneDimensionalArray.append(item)
     twoDimensionalLines.append(oneDimensionalArray)
 
+#twoDimensionalLines = [[....], [], []]
 
-
+#loop through entire 2d list
 for count, line in enumerate(twoDimensionalLines):
     #set current line in list
-    currentLine = twoDimensionalLines[count][0]
+    currentLine = twoDimensionalLines[count]
     #set next line in list accounting for index range
     if count + 1 < len(twoDimensionalLines):
         nextLine = twoDimensionalLines[count + 1]
     
-    #loop through the item (string) in current line and look for special characters
-    for count, character in enumerate(currentLine):
-        if character not in notSpecial:
-            #Initialise empty string and loop through characters
-            i = 1
-            numString = ''
-            while currentLine[count + i].isdigit() and (count + 1) + i < len(currentLine):
-                numString += currentLine[count + i]
-                i += 1
-                if currentLine[count + i].isdigit() == False:
-                    partNumberSum += int(numString)
-                    break
+    #split current line on digits and look through items until a whole numbers is found
+    #then check if adjacent characters are special symbols
+    currentLineSplitOnDigits = digitFinder(currentLine)
+    for count, item in enumerate(currentLineSplitOnDigits):
+        if (count + 1) in range(len(currentLineSplitOnDigits)):
+            print(str(currentLineSplitOnDigits[count+1]))
+        if item.isdigit() and currentLineSplitOnDigits[count + 1] not in notSpecial:
+            partNumberSum += int(item)
+        if item.isdigit() and currentLineSplitOnDigits[count - 1] not in notSpecial:
+            partNumberSum += int(item)
 
-            j = 1
-            numString = ''
-            while currentLine[count - j].isdigit() and (count - 1) - j < len(currentLine):
-                numString += currentLine[count - j]
-                j += 1
-                if currentLine[count - j].isdigit() == False:
-                    partNumberSum += int(numString)
-                    break
+#print(partNumberSum)
 
-print(partNumberSum)
-            
-            
+# testString = ['01234567']
+# print(testString[0][0])
